3f5d10497074277117674f034614fd82ba4b5a8d
Akul Singhal <singhal.akul9@gmail.com>
2021-04-19 15:05:50 +0530

    add cost, plots and multiple random splits

--- Logistic_Regression/driver.py
+++ Logistic_Regression/driver.py
@@ -3,23 +3,24 @@ from sgd import *
 from helper import *
 import time
 import sys
+import matplotlib.pyplot as plt
 
 start = time.time()
 tot_accuracy = 0
-r = np.random.RandomState(1)
+r = np.random.RandomState(1024)
+algo = sys.argv[1]
+print(algo)
 for i in range(10):
-    algo = sys.argv[1]
     x_train, y_train, x_test, y_test = train_test_split(df, split = 0.7, randomState = r)
-    print(algo)
-    w, b = gradient_descent(x_train, y_train, 0.1) if (algo == 'GD') else stochastic_gradient_descent(x_train, y_train, 0.001)# if (algo == 'sgd') else print('error'), exit(0))
+    w, b = gradient_descent(x_train, y_train, 5, 1000) if (algo == 'GD') else stochastic_gradient_descent(x_train, y_train, 0.001, 200)# if (algo == 'sgd') else print('error'), exit(0))
     y_pred = predict(x_train, y_train, w, b)
     accuracy = calcAccuracy(y_pred, y_train)
-    # print("Training Accuracy : ", accuracy)
+    print(f"Training Accuracy (Random Split #{i+1}): ", accuracy)
     y_pred = predict(x_test, y_test, w, b)
     accuracy = calcAccuracy(y_pred, y_test)
     tot_accuracy+=accuracy
-    print("Testing Accuracy : ", accuracy)
-
+    print(f"Testing Accuracy (Random Split #{i+1}): ", accuracy)
+plt.show()
 end = time.time()
 print(f"Runtime of the program is {end - start}")
 print("Testing Accuracy (10 random splits) : ", tot_accuracy/10)
--- Logistic_Regression/gd.py
+++ Logistic_Regression/gd.py
@@ -15,6 +15,15 @@ def descent(W, b, x_train, y_train, alpha):
 def gradient_descent(x_train, y_train, alpha=0.1, num_iter=2000):
     W = np.zeros(x_train.shape[1])
     b = 0
+    costs = []
+    iterations = []
     for iter in range(num_iter):
         W, b = descent(W, b, x_train, y_train, alpha)
+        if iter % 50 ==0:
+            currCost = cost(x_train, y_train, W,b)
+            costs.append(currCost)
+            iterations.append(iter)
+            # print(f"Cost at iteration #{iter}: {currCost}")
+    plt.clf()
+    plt.plot(iterations, costs)
     return W, b
