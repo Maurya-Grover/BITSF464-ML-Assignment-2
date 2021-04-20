b5ceff760afe8bc7e86e674b88af2f8368a535b5
Akul Singhal <singhal.akul9@gmail.com>
2021-04-20 20:20:39 +0530

    add accuracy plot in LR

--- Logistic_Regression/gd.py
+++ Logistic_Regression/gd.py
@@ -15,15 +15,16 @@ def descent(W, b, x_train, y_train, alpha):
 def gradient_descent(x_train, y_train, alpha=0.1, num_iter=2000):
     W = np.zeros(x_train.shape[1])
     b = 0
-    costs = []
     iterations = []
+    accuracies = []
     for iter in range(num_iter):
         W, b = descent(W, b, x_train, y_train, alpha)
         if iter % 50 ==0:
-            currCost = cost(x_train, y_train, W,b)
-            costs.append(currCost)
+            y_pred = predict(x_train, y_train, W, b)
+            currAcc = calcAccuracy(y_pred, y_train)
+            accuracies.append(currAcc)
             iterations.append(iter)
             # print(f"Cost at iteration #{iter}: {currCost}")
     plt.clf()
-    plt.plot(iterations, costs)
+    plt.plot(iterations, accuracies)
     return W, b
